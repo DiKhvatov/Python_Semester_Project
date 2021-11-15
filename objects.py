ef9af8f6750e758004c9c8043c99dd3fb690e150
Someone Rude <example@mail.com>
2021-11-15 22:21:00 +0300

    writing classes, writing visual. there are many thungs to do

--- objects.py
+++ objects.py
@@ -1,46 +1,103 @@
 import numpy as np
 import pygame as pg
 
+
+class Bullet:
+    def __init__(self, x, y, shooting_angle, v):
+        self.x = x
+        self.y = y
+        self.angle = shooting_angle
+        self.v = v
+        self.existion = True
+
+    def checking_breakthrough(self, tanks):
+        for tank in tanks:
+            distance = np.sqrt((self.x - tank.x) ** 2 + (self.y - tank.y) ** 2)
+            if tank.r > distance:
+                tank.breakthrough()
+                self.existion = False
+
+
 class Tank:
+    """
+    Класс танка
+    x,y - координаты
+    angle - угол поворота в двумерном пространстве
+    v, a - скороть и ускорение вдоль направления танка
+    r - радиус танка, пока мы не решили вопрос с его отрисовкой
+    color - цвет танка
+
+    Надо доделать отрисовку на поверхность, которую будем поворачивать и рисовать на основной экран
+    """
+
     def __init__(self):
+        """
+        инициализация
+        """
         self.x = 0
         self.y = 0
         self.v = 0
         self.a = 0
         self.angle = 0
         self.r = 5
-        self.color = (255,255,255)
+        self.color = (255, 255, 255)
 
     def move(self, delta):
+        """
+        Изменения скорости, координат и угла поворота за малое время delta
+        """
         self.v += self.a * delta
         self.x += self.v * delta * np.cos(angle)
         self.y += self.v * delta * np.sin(angle)
         self.angle += self.w * delta
 
     def start_acceleration_forward(self, acceleration):
+        """
+        Предположительно мы нажимаем стрелочку вперед и запускаем эту функцию, но теперь мне кажется это немного туповатым
+        """
         self.a = acceleration
 
     def start_acceleration_back(self, acceleration):
-        self.start_acceleration_forward(- acceleration)
+        self.start_acceleration_forward(-acceleration)
 
     def end_acceleration_forward(self, acceleration):
         self.a = 0
 
     def end_acceleration_back(self, acceleration):
-        self.start_acceleration_forward(- acceleration)
+        self.start_acceleration_forward(-acceleration)
 
     def start_rotate_right(self, w):
-        self.angle = w
+        self.w = w
 
     def end_rotate_right(self, w):
-        self.angle = 0
+        self.w = 0
 
     def start_rotate_left(self, w):
-        self.angle = w
+        self.w = w
 
     def end_rotate_left(self, w):
-        self.angle = 0
+        self.w = 0
 
-    def draw(self, screen):
-        # TODO: надо как-нибудь классненько с поворотами
+    def draw(self, surface):
+        # TODO: что-то мне лень пока что
         pass
+
+
+class Target:
+    def __init__(self):
+        # TODO: класс
+        self.x = 0
+        self.y = 0
+
+
+class Target_shooting(Target):
+    def __init__(self):
+        super().__init__(self)
+        self.shooting_angle = 0
+
+    def aiming(self, tank):
+        distance = np.sqrt((self.x - tank.x) ** 2 + (self.y - tank.y) ** 2)
+        self.shooting_angle = np.arctan((tank.x - self.x) / distance)
+
+    def shoot(self):
+        return Bullet(self.x, self.y, self.shooting_angle, 10)
--- visual.py
+++ visual.py
@@ -4,10 +4,19 @@ import thorpy
 window_height = 1000
 window_width = 1000
 
+"""
+Есть идея засунуть все связанное с рисованием в этот файл
+"""
+
+
 class Drawer:
+    """
+    Стыренная функция из проекта солнечной системы
+    Надо бы
+    """
+
     def __init__(self, screen):
         self.screen = screen
-
 
     def update(self, figures, ui):
         self.screen.fill((0, 0, 0))
@@ -20,7 +29,7 @@ class Drawer:
 
 
 def init_ui(screen):
-    global browser
+
     button_stop = thorpy.make_button("Quit", func=stop_execution)
     button_pause = thorpy.make_button("Pause", func=pause_execution)
     button_play = thorpy.make_button("Play", func=start_execution)
@@ -28,20 +37,15 @@ def init_ui(screen):
     score = thorpy.OneLineText("Score: ")
     button_new_game = thorpy.make_button(text="New game", func=new_game)
 
-    box = thorpy.Box(elements=[
-        button_pause,
-        button_stop,
-        button_play,
-        button_new_game,
-        timer,
-        score])
-
+    box = thorpy.Box(
+        elements=[button_pause, button_stop, button_play, button_new_game, timer, score]
+    )
 
     menu = thorpy.Menu(box)
     for element in menu.get_population():
         element.surface = screen
 
-    box.set_topleft((0,0))
+    box.set_topleft((0, 0))
     box.blit()
     box.update()
     return menu, box, rounds, score
