93496e42192b949f91d8de9cdacbf7a869b8dd8f
Someone Rude <example@mail.com>
2021-12-09 17:07:12 +0300

    adding docstrings to objects and visual

--- objects.py
+++ objects.py
@@ -7,11 +7,29 @@ global world_right
 global world_up
 global world_down
 
+'''
+Файл с описание классов элементов
+Bullet - класс пуль
+Tank - класс танков
+Target - класс цели
+Target_shooting - класс стреляющей цели с наследованием от простого класса цели
+'''
+
 class Bullet:
     '''
-
+    Класс пули
     '''
     def __init__(self, x, y, shooting_angle, v, parent):
+        '''
+        Инициализация
+        x, y - положение пули
+        angle - направление движения
+        v - скорость пули
+        existion - флаг существования пули с неправильным написанием
+        color - цвет пули
+        r - размер пулм
+        parent - метка класса создателя пули, чтобы она этот класс не трогала
+        '''
         self.x = x
         self.y = y
         self.angle = shooting_angle
@@ -23,6 +41,9 @@ class Bullet:
         self.parent = parent
 
     def checking_breakthrough(self, tanks):
+        '''
+        функция проверки столкновения с элементами из массива
+        '''
         for tank in tanks:
             distance = np.sqrt((self.x - tank.x) ** 2 + (self.y - tank.y) ** 2)
             if tank.r > distance:
@@ -30,16 +51,26 @@ class Bullet:
                 self.existion = False
 
     def move(self, delta):
+        '''
+        Функция перемещения пули за время delta
+        '''
         self.x += self.v * np.cos(self.angle) * delta
         self.y += self.v * np.sin(self.angle) * delta
 
     def draw(self, surface, x, y):
+        '''
+        Функция отрисовки пули на поверхности в системе отсчета танка игрока
+        surface - поверхность для отрисовки
+        x,y - подающиеся координаты
+        '''
         pg.draw.circle(surface, self.color, (self.x - x, self.y - y), self.r)
 
     def wall_collision(self, wall_x_left, wall_x_right, wall_y_up, wall_y_down):
+        '''
+        Проверка на выход пуль за пределы карты
+        '''
         if self.x < wall_x_left or self.x > wall_x_right or self.y < wall_y_up or self.y > wall_y_down:
             self.existion = False
-
 
 
 class Tank:
@@ -50,8 +81,6 @@ class Tank:
     v, a - скороть и ускорение вдоль направления танка
     r - радиус танка, пока мы не решили вопрос с его отрисовкой
     color - цвет танка
-
-    Надо доделать отрисовку на поверхность, которую будем поворачивать и рисовать на основной экран
     """
 
     def __init__(self):
@@ -70,6 +99,7 @@ class Tank:
         self.health = 100
         self.existion = True
 
+
     def move(self, delta):
         """
         Изменения скорости, координат и угла поворота за малое время delta
@@ -83,7 +113,12 @@ class Tank:
         while self.angle < 0:
             self.angle += 2*np.pi
 
+
     def draw(self, surface, x, y, screen):
+        '''
+        Отрисовка картинки танка
+        автор - Батухан
+        '''
         image = pg.image.load('floppa.png').convert_alpha()
         new_image = pg.transform.scale(image, (2*self.r, 2*self.r))
         new_image = pg.transform.rotate(new_image, -90 - 180*self.angle/np.pi)
@@ -95,32 +130,51 @@ class Tank:
         screen.blit(new_image, (self.x - x - 1.2*self.r - deltaX, self.y - y - 1.2*self.r - deltaY))
 
 
-
     def wall_collision(self, delta):
+        '''
+        Просчет выхода танка за границы мира и уменьшение его здоровья
+        '''
         if self.x > world_right or self.x < world_left or self.y > world_down or self.y < world_up:
             self.health += -0.1 * delta
-            print(self.health)
-
+            #print(self.health)
+            pass
 
 
     def breakthrough(self):
+        '''
+        Функция, сообщающая танку, что в него попала пуля
+        '''
         self.health += -1
         if self.health <= 0:
             self.existion = False
 
+
     def new_round(self):
+        '''
+        Обновление координат и скорости при начале нового раунда
+        '''
         self.x = 0
         self.y = 0
         self.v = 0
 
+
     def target_collision(self, target):
+        '''
+        Проверка, сталкивается ли танк с целями
+        '''
         if (self.x - target.x) ** 2 + (self.y - target.y) ** 2 <= (self.r + target.r) ** 2:
             self.health -= 10
             target.existion = False
-            
+
 
 class Target:
+    '''
+    Класс цели
+    '''
     def __init__(self, x, y, v):
+        '''
+
+        '''
         self.x = x
         self.y = y
         self.v = v
@@ -136,16 +190,24 @@ class Target:
 
 
     def aiming(self, tank):
+        '''
+        Высокоинтелеллектуальный алгоритм преследования танка
+        '''
         distance = np.sqrt((self.x - tank.x) ** 2 + (self.y - tank.y) ** 2)
         self.angle = np.arccos((tank.x - self.x) / distance)
 
         if -(self.y - tank.y) < 0:
             self.angle = 2 * np.pi - self.angle
 
+
     def breakthrough(self):
+        '''
+        Сообщение цели, что в нее попала пуля
+        '''
         self.health += -1
         if self.health <= 0:
             self.existion = False
+
 
     def move(self, delta):
         """
@@ -155,20 +217,29 @@ class Target:
         self.x += self.v * delta * np.cos(self.angle)
         self.y += self.v * delta * np.sin(self.angle)
         self.angle += self.w * delta
-        
+
 
     def draw(self, surface, x, y, screen):
+        '''
+        Функция отрисовки цели
+        автор - Батухан
+        '''
         image = pg.image.load('floppa2.png').convert_alpha()
         new_image = pg.transform.scale(image, (2*self.r, 2*self.r))
         new_image = pg.transform.rotate(new_image, -90 - 180*self.angle/np.pi)
         screen.blit(new_image, (self.x - x - 1.2*self.r, self.y - y - 1.2*self.r))
 
+
 class Target_shooting(Target):
+    '''
+    Класс стреляющей цели с наследованием от обычной
+    '''
     def __init__(self, x, y, v):
         super().__init__(x, y, v)
         self.shooting_angle = 0
         self.charge = 10
         self.type = "shooting target"
+
 
     def shoot_aiming(self, tank):
         distance = np.sqrt((self.x - tank.x) ** 2 + (self.y - tank.y) ** 2)
@@ -177,11 +248,18 @@ class Target_shooting(Target):
         if -(self.y - tank.y) < 0:
             self.shooting_angle = 2 * np.pi - self.shooting_angle
 
+
     def charging(self, delta):
+        '''
+        зарядка для последующего выстрела
+        '''
         self.charge += delta
 
 
     def shoot(self):
+        '''
+        Выстрел
+        '''
         global v_bullet
         if self.charge >= 10:
             self.charge = 0
